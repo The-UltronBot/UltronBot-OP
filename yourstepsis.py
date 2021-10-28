d2eb183802fb0260f1465aad54f7786af94eb009
♡мιcкεү♡ <88245294+MickeyxD@users.noreply.github.com>
2021-10-28 23:19:47 +0530

    Add files via upload

---
+++ yourstepsis.py
@@ -0,0 +1,11 @@
+from os import system
+
+system(
+    "git clone https://gitHub.com/kaal0408/Lucifer-X && cd Lucifer-X && python3 -m Lucifer"
+)
+# if got error
+print(
+    " Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Lucifer_support_group"
+)
+# Script written for stop heroku suspension
+# use with credit else gay
