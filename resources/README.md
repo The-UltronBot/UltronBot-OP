cf179a2a947e87e97584d3c19c05ee39ba42c23e
♡мιcкεү♡ <88245294+MickeyxD@users.noreply.github.com>
2021-10-29 10:33:31 +0530

    Add files via upload

---
+++ resources/README.md
@@ -0,0 +1,4 @@
+## (c) 2020 Lucifer
+# Do not edit
+Files for proper functioning of the userbot.
+Kang with credit else ready to get gban 🥺🥺
---
+++ resources/termux_session_gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+echo "
+
+LUCIFER
+
+"
+echo Starting dependency installation in 5 seconds...
+sleep 5
+apt-get update
+apt-get upgrade -y
+pkg upgrade -y
+pkg install python wget -y
+wget https://raw.githubusercontent.com/kaal0408/Lucifer-X/main/Lucifer-setup.py
+pip install telethon
+python Lucifer-setup.py
